0dc693c57773379548ca7101c1fee978e3c4cea4
Barry Yu <doung1118@gmail.com>
2021-01-25 03:38:14 +0800

    Updata: create folder : screens // cause it only read this // and than CLI: Rnfe> import components , insert app.js , show page awesome

--- App.js
+++ App.js
@@ -4,15 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from "./screens/LoginScreen"
 export default function App() {
 
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
-            <View style={styles.container}>
-          <Text>OBarry test </Text>
-          </View>
+        <Stack.Screen name='Login' component={LoginScreen} />
        </Stack.Navigator>
 </NavigationContainer>
   );
---
+++ screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { StyleSheet} from 'react-native';
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text> I am Login screen wiht Barry  </Text>
+    </View>
+  )
+}
+
+export default LoginScreen 
+
+const Styles = StyleSheet.create({}); 
