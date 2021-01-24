21e4f166710f5f134ecbd991d59b8ef61ee3b481
Barry Yu <doung1118@gmail.com>
2021-01-25 03:25:33 +0800

    firest git Native-app-chat-start

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 export default function App() {
+
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+            <View style={styles.container}>
+          <Text>OBarry test </Text>
+          </View>
+       </Stack.Navigator>
+</NavigationContainer>
   );
 }
 
