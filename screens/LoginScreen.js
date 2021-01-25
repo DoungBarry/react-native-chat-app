a4b96b6038dda3ffd8d3665df644b60fa66a29a5
Barry Yu <doung1118@gmail.com>
2021-01-25 10:32:53 +0800

    Add : Input with inline CSS

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -8,12 +8,19 @@ const LoginScreen = () => {
       <Image
         style={{ width: 300, height: 300 }}
         source={{ uri: "https://www.barrysjewellers.com/skin/frontend/boilerplate/barrys/images/logo-2020.jpg" }}
-        
-
       />
-<Text >Hello 2021 Eiji go away party </Text>
-          
-    </View>
+     
+ 
+      <Input
+        placeholder="Email"
+        autoFocus
+        value={email}
+        onChangeText={ (text => setEmail(text))}
+        value={email}
+        type="email"  
+        /> 
+      
+         </View>
   )
 }
 
