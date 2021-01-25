b442a91aa163e4b7bf5d324f3d5e427163b7ab40
Barry Yu <doung1118@gmail.com>
2021-01-25 10:22:27 +0800

    updata

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,8 +6,10 @@ const LoginScreen = () => {
   return (
     <View >
       <Image
-  source={{ uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Amazon.com-Logo.svg/800px-Amazon.com-Logo.svg.png" }}
-  style={{ width: 200, height: 200 }}
+        style={{ width: 300, height: 300 }}
+        source={{ uri: "https://www.barrysjewellers.com/skin/frontend/boilerplate/barrys/images/logo-2020.jpg" }}
+        
+
       />
 <Text >Hello 2021 Eiji go away party </Text>
           
