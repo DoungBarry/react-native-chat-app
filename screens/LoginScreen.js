e9bca72dd6ce4d3869ec4f6143f66640a90bb23c
Barry Yu <doung1118@gmail.com>
2021-01-27 00:55:40 +0800

    fixed : keyboardAvoidView not working help me

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,76 +1,92 @@
 import React, { useState } from 'react'
 import { Image, Button, Input } from "react-native-elements";
-import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
+import { KeyboardAvoidingView, StyleSheet, Text, View, Keyboard } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
 
 // Join keyboarAvoidingview is keyboard show below , when you click screen //
 
-const LoginScreen = () => { 
+const LoginScreen = () => {
 
-  const [email, setEmail ] = useState("") ;
+  const [email, setEmail] = useState("");
 
-  const [password, setPassword]=useState("");
+  const [password, setPassword] = useState("");
      
 
 
   //tell your input this is useState ! // 
 
+  const signIn=()=> {};
+
 
   return (
     
-    <View style={Styles.container}>
+    <KeyboardAvoidingView behavior="padding"  style={styles.container}>
+
+      <StatusBar style="light" />
       
+
       <Image
         style={{ width: 200, height: 200 }}
         source={{ uri: "/Users/doung1118/Code/ReactNative/message-chat-native/assets/logo/logo.png" }}
       />
-      <Input
+
+
+
+
+      <View style={styles.inputContainer}>
+
+      <Input 
         placeholder="Email"
           autoFocus
           type="email"  
           value={email}
         value={email}
-        onChangeText={ (text) => setEmail(text)}
- 
-        
+        onChangeText={ (text) => setEmail(text)}  
       /> 
         
       {/* set email and setEmail of Variable  */}
-
-
     <Input
         placeholder="Password"
           secureTextEntry
           type="email"  
         value={password}
         onChangeText={ (text )=> setPassword(text)}
-       
       /> 
 
-      <Button containerStyle={Styles.button} title="V - up" />
+      <Button containerStyle={styles.button} title="V-up" />
 
-      <Button containerStyle={Styles.button} title="Register" type="outline " />
+      <Button containerStyle={styles.button} title="Register" type="outline " />
       
-          <KeyboardAvoidingView behavior="padding" enabled style={Styles.container}>
-      </KeyboardAvoidingView>
-      
-   </View>
+          {/* <KeyboardAvoidingView behavior="padding" enabled style={Styles.container}>
+      </KeyboardAvoidingView> */}
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
 export default LoginScreen 
 
-const Styles = StyleSheet.create({  
+const styles = StyleSheet.create({  
   container: {
     flex:1,
     alignItems: "center",
     // justify-content: "",
-    alignItems:"center",
+    alignItems: "center",
+    padding: 20, 
+    backgroundColor: "white"
+    
   },
 
+
+  inputContainer: {
+    width :200 ,
+  } ,
+
   button: {
-    // width: 200,
-    // marginTop:10 , 
-   }
-   
+    width: 200,
+    marginTop: 10, 
+    
+} 
 })
 
